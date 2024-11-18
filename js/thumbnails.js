599219d2f69222f5707fc0b4f0d498a8c61a5899
Julia Vostrikova <kosheen67@gmail.com>
2024-11-18 20:13:48 +0300

    Module 7, task1

---
+++ js/thumbnails.js
@@ -0,0 +1,27 @@
+import {createPhotoDataArray} from './data.js';
+
+const picturesContainer = document.querySelector('.pictures');
+
+
+const pictureLinkTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const picturesListFragment = document.createDocumentFragment();
+
+const photoDataArray = createPhotoDataArray();
+
+
+photoDataArray.forEach((element) => {
+
+  const pictureElement = pictureLinkTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = element.url;
+  pictureElement.querySelector('.picture__img').src = element.url;
+  console.log(pictureElement);
+
+  pictureElement.querySelector('.picture__img').alt = element.description;
+
+  pictureElement.querySelector('.picture__comments').textContent = element.comments;
+  pictureElement.querySelector('.picture__likes').textContent = element.likes;
+  picturesListFragment.append(pictureElement);
+});
+
+picturesContainer.append(picturesListFragment);
